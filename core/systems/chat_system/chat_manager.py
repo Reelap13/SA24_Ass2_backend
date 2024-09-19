b7661211d96fac52f67ab6adb8ecde3089fc8b76
AlexeyKureykin <59525932+AlexeyKureykin@users.noreply.github.com>
2024-09-19 16:09:59 +0300

    Code style change

--- core/systems/chat_system/chat_manager.py
+++ core/systems/chat_system/chat_manager.py
@@ -1,26 +1,22 @@
 from typing import List
 
-from .chat_saver import *
+from .chat_saver import load_messages, save_messages
 from .message import Message
 
-class ChatManager():
+
+class ChatManager:
     def __init__(self):
         self.messages: List[Message] = load_messages()
 
-
-    async def add_message(
-            self,
-            message: Message
-    ):
+    async def add_message(self, message: Message):
         self.messages.append(message)
         save_messages(self.messages)
-
 
     async def get_messages(self) -> dict:
         return [message.dict() for message in self.messages]
 
-
-    async def get_messages_count(self) -> dict:
+    async def get_messages_count(self) -> int:
         return len(self.messages)
+
 
 chat = ChatManager()
