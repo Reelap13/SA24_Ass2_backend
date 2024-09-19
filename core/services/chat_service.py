58748774a4a66d0b9a9086b37e78dea2785e6f94
AlexeyKureykin <59525932+AlexeyKureykin@users.noreply.github.com>
2024-09-19 15:58:33 +0300

    Code style change

--- core/services/chat_service.py
+++ core/services/chat_service.py
@@ -1,27 +1,21 @@
 from datetime import datetime
 
 from schemas.chat_scheme import SendMessageScheme
-from systems.chat_system import chat, Message 
+from systems.chat_system import chat, Message
 
 
 async def send_message(message: SendMessageScheme) -> dict:
-
     current_time = datetime.now().isoformat()
     print(current_time)
     await chat.add_message(Message(text=message.text, timestamp=current_time))
-
     return {'message': 'Message was sent'}
 
 
 async def get_messages() -> dict:
-    
     messages = await chat.get_messages()
-    
     return {'messages': f'{messages}'}
 
 
 async def get_messages_count() -> dict:
-
     count = await chat.get_messages_count()
-
     return {'count': f'{count}'}
